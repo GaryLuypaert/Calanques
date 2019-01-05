a76b5e89eb9081a3f301053022ea6d77cb902dad
Gary Luypaert <g.luypaert@me.com>
2019-01-05 17:37:38 +0100

    Delete an element + Reload button

--- Calanques/Controllers/ControllerWithTableView.swift
+++ Calanques/Controllers/ControllerWithTableView.swift
@@ -51,4 +51,18 @@ class ControllerWithTableView: UIViewController, UITableViewDelegate, UITableVie
         }
     }
     
+    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete {
+            // Delete the row from the data source
+            calanques.remove(at: indexPath.row)
+            tableView.deleteRows(at: [indexPath], with: .fade)
+        }
+    }
+    
+    @IBAction func reloadAction(_ sender: Any) {
+        calanques = CalanqueCollection().all()
+        tableView.reloadData()
+    }
+    
+    
 }
